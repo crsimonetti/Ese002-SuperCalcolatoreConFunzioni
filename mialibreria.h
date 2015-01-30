536e1f1124c39fd32aedda8757be0508d2fbd708
Ciro Simonetti <ciro.simonetti207@gmail.com>
2015-01-30 09:45:44 +0100

    PRIMO COMMIT

---
+++ mialibreria.h
@@ -0,0 +1,14 @@
+/* 
+ * File:   mialibreria.h
+ * Author: studente
+ *
+ * Created on 30 gennaio 2015, 9.43
+ */
+
+float somma(float a, float b);
+float sottrazione(float a, float b);
+float prodotto(float a, float b);
+float divisione(float a, float b);
+int quoziente(int a, int b);
+float frazionale(int a, int b);
+float potenza(float base, int esponente);
